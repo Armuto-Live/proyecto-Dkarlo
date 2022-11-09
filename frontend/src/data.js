04e259bcb1e912ee9f7c7e6878957558890a322c
Armuto-Live <luisarmuto3@gmai.com>
2022-11-09 11:37:08 -0500

    create categoryItems and categories

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -21,3 +21,19 @@ export const sliderItems=[
         bg:"fbf0f4",
     },
 ]
+export const categories=[
+    {
+        id:1,
+        imagen:"https://i.ibb.co/nzyVXrz/categoria-adidas.jpg",
+        title:"Zapatillas deportivas",
+        desc:"Descuento de 10%",
+        bg:"fcf1ed",
+    },
+    {
+        id:2,
+        imagen:"https://i.ibb.co/qJNfZBX/categoria-puma.jpg",
+        title:"Zapatillas urbanas",
+        desc:"Descuento de 20%",
+        bg:"dfdfdf",
+    },
+]
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Announcement from '../components/Announcement/Announcement'
 import Navbar from '../components/Navbar/Navbar'
 import Slider from '../components/Slider'
-
+import Categories from '../components/Categories'
 export const Home = () => {
   return (
     <div>
       <Announcement/>
       <Navbar/>
       <Slider/>
+      <Categories/>
     </div>
   )
 }
