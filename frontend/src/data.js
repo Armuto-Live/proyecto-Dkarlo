96801a892bde24cb27d33c6bc21165934d1665d7
Armuto-Live <luisarmuto3@gmai.com>
2022-11-09 09:53:40 -0500

    my first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems=[
+    {
+        id:1,
+        img:"https://i.ibb.co/VjcLsVQ/zappatilla2-sin-fondo.png",
+        title:"Zapato",
+        desc:"Descuento de 10%",
+        bg:"fcf1ed",
+    },
+    {
+        id:2,
+        img:"https://i.ibb.co/VjcLsVQ/zappatilla2-sin-fondo.png",
+        title:"Zapatilla 2",
+        desc:"Descuento de 20%",
+        bg:"dfdfdf",
+    },
+    {
+        id:3,
+        img:"https://i.ibb.co/VjcLsVQ/zappatilla2-sin-fondo.png",
+        title:"Zapato 3",
+        desc:"Descuento de 30%",
+        bg:"fbf0f4",
+    },
+]
